99c3c8b011870e1e942b9abce85636ec3a77e420
Martin Johnki <m.johnki@tarent.de>
2019-09-28 21:15:17 +0200

    optimize AgendaItemSearchUseCase

--- agendaitem/src/main/java/de/glasparlament/agendaitem/search/AgendaItemSearchUseCase.kt
+++ agendaitem/src/main/java/de/glasparlament/agendaitem/search/AgendaItemSearchUseCase.kt
@@ -6,10 +6,6 @@ import de.glasparlament.data.Transfer
 
 class AgendaItemSearchUseCase(private val repository: AgendaItemRepository) {
 
-    suspend fun execute(search: String): Transfer<List<AgendaItemSearchResult>> {
-        return when(val result = repository.searchAgendaItems(search)){
-            is Transfer.Success -> Transfer.Success(result.data)
-            is Transfer.Error -> result
-        }
-    }
+    suspend fun execute(search: String): Transfer<List<AgendaItemSearchResult>> =
+            repository.searchAgendaItems(search)
 }
