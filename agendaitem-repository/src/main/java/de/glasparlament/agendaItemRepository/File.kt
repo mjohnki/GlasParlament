da4fc6a82af40e7128ee9f761fd41a45f33073cc
Martin Johnki <m.johnki@tarent.de>
2019-09-28 17:50:19 +0200

    update model

--- agendaitem-repository/src/main/java/de/glasparlament/agendaItemRepository/File.kt
+++ agendaitem-repository/src/main/java/de/glasparlament/agendaItemRepository/File.kt
@@ -1,28 +1,7 @@
 package de.glasparlament.agendaItemRepository
 
-class File constructor(
+data class File(
         var id: String,
         var name: String,
-        var accessUrl: String) {
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as File
-
-        if (id != other.id) return false
-        if (name != other.name) return false
-        if (accessUrl != other.accessUrl) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = id.hashCode()
-        result = 31 * result + name.hashCode()
-        result = 31 * result + accessUrl.hashCode()
-        return result
-    }
-}
+        var accessUrl: String)
 
