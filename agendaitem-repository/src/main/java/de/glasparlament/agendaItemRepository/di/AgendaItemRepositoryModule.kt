d44a663a917eb7567dd76f9763fdd4a11c95879f
Martin Johnki <m.johnki@tarent.de>
2019-09-28 20:44:38 +0200

    update agendaitem repository tests

--- agendaitem-repository/src/main/java/de/glasparlament/agendaItemRepository/di/AgendaItemRepositoryModule.kt
+++ agendaitem-repository/src/main/java/de/glasparlament/agendaItemRepository/di/AgendaItemRepositoryModule.kt
@@ -1,7 +1,5 @@
 package de.glasparlament.agendaItemRepository.di
 
-import de.glasparlament.agendaItemRepository.AgendaItemApi
-import de.glasparlament.agendaItemRepository.AgendaItemEndpoint
 import de.glasparlament.agendaItemRepository.AgendaItemRepository
 import de.glasparlament.agendaItemRepository.AgendaItemRepositoryImpl
 import de.glasparlament.data.db.AgendaItemDao
@@ -10,18 +8,9 @@ import org.koin.dsl.module
 import retrofit2.Retrofit
 
 val agendaItemRepositoryModule = module {
-    single<AgendaItemEndpoint> { provideAgendaItemEndpoint(get()) }
-    single<AgendaItemApi> { provideAgendaItemApi(get()) }
     single<AgendaItemRepository> { provideAgendaItemRepository(get(), get()) }
 }
-
-fun provideAgendaItemEndpoint(retrofit: Retrofit) =
-        retrofit.create(AgendaItemEndpoint::class.java)
-
-fun provideAgendaItemApi(endpoint: AgendaItemEndpoint) =
-        AgendaItemApi(endpoint)
 
 fun provideAgendaItemRepository(agendaItemDao: AgendaItemDao,
                                 meetingDao: MeetingDao) =
         AgendaItemRepositoryImpl(agendaItemDao, meetingDao)
-
